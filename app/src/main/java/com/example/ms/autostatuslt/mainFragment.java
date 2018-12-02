bde041b71c253dbd9ce2d0aa4d428f10ec6c10c4
MantasS0 <man_tas_pats@yahoo.com>
2018-12-02 17:22:55 +0200

    Main window layout (basic) complete. Settings window layout complete with hidden renaming layout. Main fragment button navigate to settings set up with backstack.

--- app/src/main/java/com/example/ms/autostatuslt/mainFragment.java
+++ app/src/main/java/com/example/ms/autostatuslt/mainFragment.java
@@ -6,25 +6,63 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class mainFragment extends Fragment {
+
+    private Button buttonNavigateSettingsFragment;
+    private Button buttonNavigateStatisticsFragment;
+    private Button buttonAddToDatabase;
+
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_main, parent, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        buttonNavigateSettingsFragment = view.findViewById(R.id.button_settingsFragment);
+        buttonNavigateStatisticsFragment = view.findViewById(R.id.button_statisticsFragment);
+        buttonAddToDatabase = view.findViewById(R.id.button_addToDataBase);
+
+        buttonNavigateSettingsFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Going to settings", Toast.LENGTH_SHORT).show();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new settingsFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack("Settings fragment");
+                ft.commit();
+            }
+        });
+
+        buttonNavigateStatisticsFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Going to statistics", Toast.LENGTH_SHORT).show();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new settingsFragment()); //reik stats fragmento
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack("Statistics fragment");
+                ft.commit();
+            }
+        });
+
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
     }
